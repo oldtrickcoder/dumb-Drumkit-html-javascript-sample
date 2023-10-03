8cddc8e90cefbf8446e7dafb52e5f3b1873dc171
oldtrickcoder <ypratama66@gmail.com>
2023-10-03 14:36:01 +0700

    updating reflect  of drum sound

--- functional.js
+++ functional.js
@@ -21,61 +21,98 @@ const OpenHatEl = document.getElementById("OpenHat");
 const StickEl = document.getElementById("Sticks");
 
 function CrashSound() {
-  console.log("set sound");
+  audio1.pause();
+  audio1.setAttribute("src", "");
   audio1.setAttribute("src", "./assets/sounds/Acoustic Crash 04.wav");
-  console.log("play crash Sound");
   audio1.play();
+  crash.style.filter = "brightness(1.3)";
+  crash.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(crash);
+  }, 200);
 }
 function HighTomSound() {
-  console.log("set sound");
+  audio2.setAttribute("src", "");
   audio2.setAttribute("src", "./assets/sounds/Acoustic High Tom 01.wav");
-  console.log("play High Tom Sound");
   audio2.play();
+  HighT.style.filter = "brightness(1.3)";
+  HighT.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(HighT);
+  }, 500);
 }
 function Kick() {
-  console.log("set sound");
+  audio3.setAttribute("src", "");
   audio3.setAttribute("src", "./assets/sounds/Acoustic Kick 01.wav");
-  console.log("play Kick Sound");
   audio3.play();
+  KickEl.style.filter = "brightness(1.3)";
+  KickEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(KickEl);
+  }, 500);
 }
 function LowTom() {
-  console.log("set sound");
+  audio4.setAttribute("src", "");
   audio4.setAttribute("src", "./assets/sounds/Acoustic Low Tom 01.wav");
-  console.log("play Low Tom Sound");
   audio4.play();
+  LTom.style.filter = "brightness(1.3)";
+  LTom.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(LTom);
+  }, 500);
 }
 function MidTom() {
-  console.log("set sound");
+  audio5.setAttribute("src", "");
   audio5.setAttribute("src", "./assets/sounds/Acoustic Mid Tom 01.wav");
-  console.log("play Mid Tom Sound");
   audio5.play();
+  MTom.style.filter = "brightness(1.3)";
+  MTom.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(MTom);
+  }, 500);
 }
 function Snare() {
-  console.log("set sound");
+  audio6.setAttribute("src", "");
   audio6.setAttribute("src", "./assets/sounds/Acoustic Snare 01.wav");
-  console.log("play Snare Sound");
   audio6.play();
+  SnareEl.style.filter = "brightness(1.3)";
+  SnareEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(SnareEl);
+  }, 500);
 }
 function Stick() {
-  console.log("set sound");
+  audio7.setAttribute("src", "");
   audio7.setAttribute("src", "./assets/sounds/Acoustic Sticks 01.wav");
-  console.log("play Stick Sound");
   audio7.play();
+  StickEl.style.filter = "brightness(1.3)";
+  StickEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(StickEl);
+  }, 500);
 }
 function CloseHat() {
-  console.log("set sound");
+  audio8.setAttribute("src", "");
   audio8.setAttribute(
     "src",
     "./assets/sounds/hihat-Acoustic Closed Hat 01.wav"
   );
-  console.log("play Close Hat Sound");
   audio8.play();
+  CloseHatEl.style.filter = "brightness(1.3)";
+  CloseHatEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(CloseHatEl);
+  }, 500);
 }
 function OpenHat() {
-  console.log("set sound");
+  audio9.setAttribute("src", "");
   audio9.setAttribute("src", "./assets/sounds/hihat-Acoustic Open Hat 01.wav");
-  console.log("play Open Hat Sound");
   audio9.play();
+  OpenHatEl.style.filter = "brightness(1.3)";
+  OpenHatEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
+  setTimeout(() => {
+    normalize(OpenHatEl);
+  }, 500);
 }
 function normalize(el) {
   el.style.filter = "brightness(1 )";
@@ -87,75 +124,52 @@ document.body.addEventListener("keydown", function (e) {
   if (e.keyCode == 32) {
     console.log("its a space bar");
     Stick();
-    StickEl.style.filter = "brightness(1.3)";
-    StickEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(StickEl);
-    }, 500);
   }
   if (e.keyCode == 65) {
     CrashSound();
-    crash.style.filter = "brightness(1.3)";
-    crash.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(crash);
-    }, 500);
   }
   if (e.keyCode == 83) {
     HighTomSound();
-    HighT.style.filter = "brightness(1.3)";
-    HighT.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(HighT);
-    }, 500);
   }
   if (e.keyCode == 90) {
     LowTom();
-    LTom.style.filter = "brightness(1.3)";
-    LTom.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(LTom);
-    }, 500);
   }
   if (e.keyCode == 88) {
     MidTom();
-    MTom.style.filter = "brightness(1.3)";
-    MTom.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(MTom);
-    }, 500);
   }
   if (e.keyCode == 67) {
     Snare();
-    SnareEl.style.filter = "brightness(1.3)";
-    SnareEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(SnareEl);
-    }, 500);
   }
   if (e.keyCode == 70) {
     OpenHat();
-    OpenHatEl.style.filter = "brightness(1.3)";
-    OpenHatEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(OpenHatEl);
-    }, 500);
   }
 
   if (e.keyCode == 86) {
     CloseHat();
-    CloseHatEl.style.filter = "brightness(1.3)";
-    CloseHatEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(CloseHatEl);
-    }, 500);
   }
   if (e.keyCode == 68) {
     Kick();
-    KickEl.style.filter = "brightness(1.3)";
-    KickEl.style.boxShadow = "10px 10px 10px rgba(20, 20, 20, 0.05)";
-    setTimeout(() => {
-      normalize(KickEl);
-    }, 500);
   }
+});
+
+// audio1.addEventListener("load", () => {
+//   console.log("audio 1 successfully loaded");
+// });
+// document.addEventListener("load", (e) => {
+//   console.log("document loaded");
+//   console.log("The page has fully loaded");
+//   console.log(e);
+//   document.getElementById("loading-explain").style.display = "block";
+//   document.getElementById("loading").style.display = "none";
+
+//   document.getElementsByClassName("loading").style.display = "none";
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM IN loaded");
+  setTimeout(() => {
+    document.getElementById("loading-explain").style.display = "block";
+  }, 3000);
+  setTimeout(() => {
+    document.getElementById("loading").style.display = "none";
+  }, 5000);
 });
